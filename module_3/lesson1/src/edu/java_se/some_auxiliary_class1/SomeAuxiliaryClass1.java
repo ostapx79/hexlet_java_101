ddd1237197bed218483fcd1431c528cc4eb123fc
Konstantin <ostapx79@gmail.com>
2023-10-18 01:57:51 +0300

    Добавлено один класс в третим модуле

---
+++ module_3/lesson1/src/edu/java_se/some_auxiliary_class1/SomeAuxiliaryClass1.java
@@ -0,0 +1,11 @@
+package edu.java_se.some_auxiliary_class1;
+
+public class SomeAuxiliaryClass1 {
+    public static int multiply(int number) {
+        return number * number;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(multiply(5));
+    }
+}
